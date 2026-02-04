6f56121290f73c7c0a091cdb2833ea14daf3e55d
Brayemar <mogikoyob@gmail.com>
2026-02-04 14:01:29 +0300

    added python code

---
+++ lesson1c.py
@@ -0,0 +1,45 @@
+# Arithmetic operations
+# Addition
+number1 = "10"
+number2 = "20"
+addition = number1 + number2
+print("The sum of the two numbers is: ", addition)
+
+
+# subtraction
+number3 = 50
+number4 = 15
+difference = number3 - number4
+print("The diference of the numbers is:", difference)
+
+# multiplication
+number5 = 10
+number6 = 6
+product = number5 * number6
+print("The product of the number is: ", product)
+
+
+# division
+number7 = 40
+number8 = 8
+division = number5 / number6
+print("The division of the number is: ", division)
+
+
+
+# modulus - this means the remainder of a number when get subdivided by another one
+number9 = 14
+number10 = 5
+modulus = number9 % number10
+print("The modulus of the numbers is: ", modulus)
+
+# Floor Division -This is the number of items a number subdivides another ignoring the remainder
+floor = number9 // number10
+print("The floor division of the two numbers is: ", floor)
+
+
+# Exponent (**)
+# The power of a number
+number11 = 10
+exponent = number11 ** 2
+print("The exponent of the number is: ", exponent)
