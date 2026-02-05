52afbaba94b3406a3adf5338ec09c2c22e8f8aec
Brayemar <mogikoyob@gmail.com>
2026-02-05 12:10:43 +0300

    added python code

---
+++ lesson2a.py
@@ -0,0 +1,10 @@
+# Python Lists
+# A list inpython is a collection of items that are ordered in a certain way.
+# A list is introduced by the use of the square brackets []
+# The items of a list are stored inside of indexes. NOTE: In programming we start counting from index Zero(0). bmw, benze, hiance, ...
+# A list is mutable i.e the contents of a list can be changed.
+
+cars = ["BMW", "Benze", "Hiance", "Prado", "Probox", "McLaren", "Range"]
+
+print(cars)
+print(type(cars))
