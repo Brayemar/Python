7e0f33bf6946c07614cdcb0a087d11a87093d662
Brayemar <mogikoyob@gmail.com>
2026-02-11 13:18:36 +0300

    added python code

---
+++ lesson6e.py
@@ -0,0 +1,9 @@
+# On the try and except block : You run some codes and if it is successful the try block will get executed other than the except block will be executed when there is an anticipated error.
+
+
+try:
+    number = 100
+    answer = number / 0
+    print("The answer is: ",answer)
+except Exception as e:
+    print("There is an error: ", e)
